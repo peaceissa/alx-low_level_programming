7a34b4d19618480b890fc76e5277f8f34c4e6e05
Peace Love Issa <issarae78@gmail.com>
2022-07-03 07:46:20 -0700

    tASK 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,42 +6,38 @@
  * Return: ALways 0 (Success)
  *
  */
-
 int main(void)
-
-{
-
+{	
 	int n, m;
 
 	for (n = 48; n <= 56; n++)
 
 	{
-	
+
 	for (m = 49; m <= 57; m++)
 
 	{
-		
 
 	if (m > n)
-	
+
 	{
-		
+
 	putchar(n);
-	
+
 	putchar(m);
 
 	if (n != 56 || m != 57)
 
 	{
-	
-	putchar(',');													
-	
+
+	putchar(',');
+
 	putchar(' ');
 
 	}
-	
+
 	}
-	
+
 	}
 
 	}
