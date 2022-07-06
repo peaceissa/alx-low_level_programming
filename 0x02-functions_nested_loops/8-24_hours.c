74e1ae56148d7d285f771b1c4d87ceed2fe402be
Peace Love Issa <issarae78@gmail.com>
2022-07-06 06:53:31 -0700

    hot fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,22 +19,23 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(i + '0');
 			}
-			else if (i > 10)
+			else if (i >= 10)
 			{
 				   _putchar((i / 10) + '0');
 				   _putchar((i % 10) + '0');
 			}
 			if (j < 10)
 			{
-				_putchar(' : ');
+				_putchar(':');
 				_putchar('0');
 				_putchar(j + '0');
 			}
 			else if (j > 10)
 			{
-				_putchar(' : ');
+				_putchar(':');
 				_putchar('0');
 				_putchar(j % '0');
 			}
 		}
 	}
+}
