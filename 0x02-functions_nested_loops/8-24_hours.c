004eb2cca1138cb9e3484ed2326828ccc1589e15
Peace Love Issa <issarae78@gmail.com>
2022-07-06 06:17:58 -0700

    task 9 commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,10 +16,10 @@ void jack_bauer(void)
 		{
 			if (i < 10)
 			{
-				_putchar('0);
-				_putchar(i + '0')
+				_putchar('0');
+				_putchar(i + '0');
 			}
-			else if (i >= 10)
+			else if (i > 10)
 			{
 				   _putchar((i / 10) + '0');
 				   _putchar((i % 10) + '0');
