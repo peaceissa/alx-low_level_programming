058b3bc43d7b264e17b8bd0554b279d19e327fa9
Peace Love Issa <issarae78@gmail.com>
2022-07-07 01:30:37 -0700

    task 0 commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  main - tests function that prints if integer is positive or negative
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int 1;
+	i = 0;
+	positive _or_negative(i);
+	return (0);
+}
