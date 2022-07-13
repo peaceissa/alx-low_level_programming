71a3bacdef96eb87635b6af42182e3efc160e9f8
Peace Love Issa <issarae78@gmail.com>
2022-07-13 12:33:11 -0700

    task 6 fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,25 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdio.h>
 
 /**
- * cap_string - capitalizes every word of a string
- * @s: string to modify
+ * *cap_string - this is awesome
+ * @s: pointer to char params
  *
- * Return: the resulting string
+ * Return: *s
  */
 
 char *cap_string(char *s)
 {
 	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
 
-	char spe[13] = {' ', '\t', ',', ';', '.', '!', '?', '"',
-		'(', ')', '{', '}' };
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-		for (j = 0; j < 13; j++)
-		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
-		}
+		if (s[0] >= 97 && s[0] <= 122)
+			s[0] = s[0] - 32;
+				for (j = 0; delimeters[j] != '\0'; j++)
+					if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
 	}
 	return (s);
 }
