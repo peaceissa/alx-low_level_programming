070497f3bc81a869014e970f51c08a1c016d88f1
Peace Love Issa <issarae78@gmail.com>
2022-07-13 01:43:16 -0700

    task  7 commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,56 +2,58 @@
 
 /**
  * main - prints all possible different combinations of three digits
+ *
  * Return: Always 0 (Success)
  */
- 
- int main(void)
- {
- 	int n, m, 1;
-	
-	for (n = 48; n < 58; n++)
 
-	{
+int main(void)
 
-	for (m =49; m < 58; 1++)
+{
 
-	{
+		int n, m, l;
 
-	for (1 = 50; 1<58; 1++)
 
-	{
 
-	if (1 > m && m > n)
+			for (n = 48; n < 58; n++)
 
-	{
+					{
 
-	putchar(n);
+							for (m = 49; m < 58; m++)
 
-	putchar(m);
+											{
 
-	putchar(1);
+													for (l = 50; l < 58; l++)
 
-	if (n != 55 || m != 56)
+																			{
 
-	{
+																								if (l > m && m > n)
 
-	putchar(',');
+																										{
 
-	putchar(' ');
+																									putchar(n);
 
-	}
+																									putchar(m);																																											putchar(l);
 
-	}
+																																																		if (n != 55 || m != 56)
 
-	}
+																																																								{
 
-	}
+																																																putchar(',');
 
-	}
+																																																																					putchar(' ');
 
-	putchar('\n');
-	
-	return (0);
+																																																																										}
 
-	}
+																																																						}
 
+																											}
+
+																	}
+
+									}
+
+				putchar('\n');
+
+					return (0);
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+alx-low_level_programming repo contains lessons on low level languages such as c
