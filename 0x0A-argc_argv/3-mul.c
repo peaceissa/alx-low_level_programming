ff2995bc9fe7bb6ac9b59b21102d81ccc2903a88
Peace Love Issa <issarae78@gmail.com>
2022-07-22 08:37:07 -0700

    task 3 commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * main - multipies two numbers
- * @args: n args
- * @argv: arr args
- * Return: 0
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
  */
 
-int main(int argc, char *argv)
+int _atoi(char *s)
 {
-	int i, val = 1;
+	int i, d, n, len, f, digit;
 
-	if (argc != 3)
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		printf("Error\n");
-		return (1);
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n *  10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
 	}
-	for (i = i; i < argc; i++)
-	{
-		val *= atoi(argv[i]);
-	}
-	printf("%d\n", val);
-	return (0);
+	if (f == 0)
+		return (0);
+	return (n);
 }
